c56188a02dc169e8cb303c460a1a6b0f2bc12418
a-gh-a <gh630602@gmail.com>
2025-04-25 23:07:08 +0330

    phase1.2

--- p1.cpp
+++ p1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <regex>
+#include <ctime>
 
 using namespace std;
 
@@ -115,6 +118,157 @@ public:
 
 
 
+
+
+class Student {
+private:
+    int student_id;
+    string name, email;
+    float balance;
+    vector<int> reservations;
+
+public:
+
+
+    void print() const {
+        cout << "Student: " << name << ", Email: " << email << ", Balance: " << balance << endl;
+    }
+
+    void input()
+    {
+     set_student_id();
+     set_name();
+     set_email();
+     set_balance();
+    }
+
+    bool reserve_meal(int meal_id, float price) {
+        if (balance >= price) {
+            reservations.push_back(meal_id);
+            balance -= price;
+            return true;
+        }
+        cout<<"erorr ";
+        return false;
+    }
+
+    bool cancel_reservation(int meal_id) {
+        for (auto it = reservations.begin(); it != reservations.end(); ++it) {
+            if (*it == meal_id) {
+                reservations.erase(it);
+                return true;
+            }
+        }
+        std::cerr << "رزرو یافت نشد!\n";
+        return false;
+    }
+
+    // Setters with validation
+    void set_student_id() { cout<<"enter student_id:";cin>>student_id;  }
+    void set_name() { cout<<"enter student_name:"; cin>>name; }
+    void set_email() {
+
+        for(int i=0;i<1;)
+            {
+        cout<<"enter email:";
+        cin>>email;
+        regex email_pattern(R"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})");
+
+        if (regex_match(email, email_pattern)) i++;
+        else cout<<"email is not correct";
+            }
+                     }
+
+
+    void set_balance() {
+        for(int i=0;i<1;)
+            {
+                cout<<"enter balance:";
+                cin>>balance;
+                if (balance > 0) { i++;}
+
+
+                 else cout<<endl<<"enter again";
+            }
+
+    }
+
+    // Getters
+    int get_student_id() const { return student_id; }
+    string get_name() const { return name; }
+    string get_email() const { return email; }
+    float get_balance() const { return balance; }
+};
+
+
+
+enum ReservationStatus { SUCCESS, CANCELLED, FAILED };
+
+class Reservation {
+private:
+    int reservation_id;
+    Student studentt;
+    M* meal;
+    din* hall;
+    ReservationStatus status;
+    time_t created_at;
+
+public:
+  //  Reservation(int id, Student* s, Meal* m, DiningHall* h) {
+    //    set_reservation_id(id);
+      //  set_student(s);
+        //set_meal(m);
+       // set_hall(h);
+       // set_status(SUCCESS);
+        //created_at = time(nullptr);
+    //}
+
+    void print() const {
+        cout << "Reservation ID: " << reservation_id << ", Student: " << studentt.get_name()
+                  << ", Meal: " << meal->get_name() << ", Status: " << status << endl;
+    }
+
+    void input(Student b2){
+        set_reservation_id();
+        set_student(b2);
+        set_meal();
+        set_hall();
+        set_status();
+                }
+
+    void cancel() {
+        if (status == SUCCESS) status=CANCELLED;
+        else cout<<"cansel bod";
+
+                  }
+
+    // Setters with validation
+    void set_reservation_id() {cout<<"enter re_id:"; cin>>reservation_id; }
+    void set_student(Student b1) {studentt=b1;}
+    void set_meal() { cout<<"enter meal:"; }
+    void set_hall() { cout<<"enter hall:";}
+    void set_status() {
+        int i;
+        cout<<"enter re_status:";
+        cin>>i;
+        if(i==1)status=SUCCESS;
+        if(i==2)status=CANCELLED;
+        if(i==3)status=FAILED;
+
+      }
+
+    // Getters
+    int get_reservation_id() const { return reservation_id; }
+    Student get_student() const { return studentt; }
+    M* get_meal() const { return meal; }
+    din* get_hall() const { return hall; }
+    ReservationStatus get_status() const { return status; }
+    time_t get_created_at() const { return created_at; }
+};
+
+
+
+
 int main(){
 
  M hj;
@@ -126,6 +280,16 @@ din k;
 
 k.input();
 k.print();
+
+Student b;
+
+b.input();
+b.print();
+
+Reservation l;
+
+l.input(b);
+l.print();
 
 return 0;
 }
