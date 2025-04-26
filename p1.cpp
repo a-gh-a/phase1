063e2efb386df7825b8c9c06b09f44f174daccc3
a-gh-a <gh630602@gmail.com>
2025-04-26 20:59:58 +0330

    phase1.3

--- p1.cpp
+++ p1.cpp
@@ -33,20 +33,20 @@ public:
 
     // Setters with validation
     void set_meal_id() {
-        cout<<"enter id:";
+        cout<<"enter meal_id:";
         cin>>meal_id;
          }
 
 
     void set_name() {
-        cout<<"enter name:";
+        cout<<"enter meal_name:";
         cin>>name;
          }
 
 
     void set_type() {
          int i;
-         cout<<"enter type meal:";
+         cout<<"enter type meal:(1,2,3)";
          cin>>i;
           if(i==1) type=BREAKFAST;
           if(i==2) type=LUNCH;
@@ -208,31 +208,24 @@ class Reservation {
 private:
     int reservation_id;
     Student studentt;
-    M* meal;
-    din* hall;
+    M meall;
+    din halll;
     ReservationStatus status;
     time_t created_at;
 
 public:
-  //  Reservation(int id, Student* s, Meal* m, DiningHall* h) {
-    //    set_reservation_id(id);
-      //  set_student(s);
-        //set_meal(m);
-       // set_hall(h);
-       // set_status(SUCCESS);
-        //created_at = time(nullptr);
-    //}
+
 
     void print() const {
         cout << "Reservation ID: " << reservation_id << ", Student: " << studentt.get_name()
-                  << ", Meal: " << meal->get_name() << ", Status: " << status << endl;
+                  << ", Meal: " << meall.get_name() << ", Status: " << status << endl;
     }
 
-    void input(Student b2){
+    void input(Student b2,M h1,din h2){
         set_reservation_id();
         set_student(b2);
-        set_meal();
-        set_hall();
+        set_meal(h1);
+        set_hall(h2);
         set_status();
                 }
 
@@ -245,11 +238,11 @@ public:
     // Setters with validation
     void set_reservation_id() {cout<<"enter re_id:"; cin>>reservation_id; }
     void set_student(Student b1) {studentt=b1;}
-    void set_meal() { cout<<"enter meal:"; }
-    void set_hall() { cout<<"enter hall:";}
+    void set_meal(M h1) {meall=h1; }
+    void set_hall(din h2) { halll=h2;}
     void set_status() {
         int i;
-        cout<<"enter re_status:";
+        cout<<"enter re_status:(1,2,3)";
         cin>>i;
         if(i==1)status=SUCCESS;
         if(i==2)status=CANCELLED;
@@ -260,8 +253,8 @@ public:
     // Getters
     int get_reservation_id() const { return reservation_id; }
     Student get_student() const { return studentt; }
-    M* get_meal() const { return meal; }
-    din* get_hall() const { return hall; }
+    M get_meall() const { return meall; }
+    din get_halll() const { return halll; }
     ReservationStatus get_status() const { return status; }
     time_t get_created_at() const { return created_at; }
 };
@@ -274,21 +267,21 @@ int main(){
  M hj;
 
 hj.input();
-hj.print();
+//hj.print();
 
 din k;
 
 k.input();
-k.print();
+//k.print();
 
 Student b;
 
 b.input();
-b.print();
+//b.print();
 
 Reservation l;
 
-l.input(b);
+l.input(b,hj,k);
 l.print();
 
 return 0;
